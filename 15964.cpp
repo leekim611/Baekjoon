effb564243c3f931912ccc80bc7667b3e57c8ea0
ire kim <leekim611@naver.com>
2022-07-21 08:47:37 +0900

    220721

---
+++ 15964.cpp
@@ -0,0 +1,17 @@
+/* 
+    풀이 날짜: 220720
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long a, b;
+    cin >> a >> b;
+    cout << (a + b) * (a - b) << endl;
+}
